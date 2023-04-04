523fe3f64d6c844e3c87b6b91dd55643e169256f
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-04-04 15:41:18 +0530

    card created

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,6 +2,20 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    return <div>NewsItem</div>;
+    let { title, description } = this.props;
+    return (
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsdetail" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
