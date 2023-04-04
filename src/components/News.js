184664458bd44f77cb083951f3538df5e92a4aa3
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-04-04 15:42:02 +0530

    creating card show in 3 col format

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,22 @@ import NewsItem from "./NewsItem";
 export default class News extends Component {
   render() {
     return (
-      <div>
-        News
-        <NewsItem />
+      <div className="container my-3">
+        <h2>Top Headline News</h2>
+
+        {/* one row inside 3*4 ka column */}
+
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="my title" description=" this is first" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" description=" this is first" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" description=" this is first" />
+          </div>
+        </div>
       </div>
     );
   }
