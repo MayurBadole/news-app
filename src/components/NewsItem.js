16b65f35aa1d83c7348fef854f27eaff7b5522fa
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-04-05 09:55:08 +0530

    creating cards

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,20 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imgurl, newsUrl } = this.props;
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src="..." className="card-img-top" alt="..." />
+          <img src={imgurl} className="card-img-top" alt="img " />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-primary">
-              Go somewhere
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Read More
             </a>
           </div>
         </div>
